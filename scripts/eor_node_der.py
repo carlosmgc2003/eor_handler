56d69b15ac4683cb2bbe4c54c06b3b4948a83fbd
Carlos MAceira <carlosmgc2003@gmail.com>
2020-12-22 17:19:48 -0300

    Version que funciona en el robot

--- scripts/eor_node_der.py
+++ scripts/eor_node_der.py
@@ -8,22 +8,22 @@ from std_msgs.msg import Int32
 
 but_pin = 29
 rate_hz = 10
-side = "derecho"
+side = "left"
 
 GPIO.setmode(GPIO.BOARD)  # BOARD pin-numbering scheme
 GPIO.setup(but_pin, GPIO.IN)  # button pin set as input
-pub = rospy.Publisher('eor_' + side, Int32, queue_size=10)
+pub = rospy.Publisher(side + '_eor', Int32, queue_size=10)
 rospy.init_node('eor_pub', anonymous=True)
 rate = rospy.Rate(rate_hz) # 10hz
 
 def eor_pub():
     while not rospy.is_shutdown():
-        if GPIO.input(but_pin) == GPIO.HIGH:
-                GPIO.wait_for_edge(but_pin, GPIO.FALLING)
+        if GPIO.input(but_pin) == GPIO.LOW:
+                #GPIO.wait_for_edge(but_pin, GPIO.RISING)
                 rospy.loginfo('EOR %s Soltado', side)
                 pub.publish(0)
-        elif GPIO.input(but_pin) == GPIO.LOW:
-                GPIO.wait_for_edge(but_pin, GPIO.RISING)
+        elif GPIO.input(but_pin) == GPIO.HIGH:
+                #GPIO.wait_for_edge(but_pin, GPIO.FALLING)
                 rospy.loginfo('EOR %s Apretado', side)
                 pub.publish(1)
         rate.sleep()
